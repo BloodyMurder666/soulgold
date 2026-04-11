2bdd329ed2ad27b86782e59df4f655298da337da
Eemeliri <eemeli.rintanen@gmail.com>
2026-04-11 21:52:20 +0300

    whitney gym normal and hardmode

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -203,7 +203,7 @@
 #define VAR_SLATEPORT_FAN_CLUB_STATE                     0x40B7
 #define VAR_PLAYER_STARTER_SPECIES                           0x40B8
 #define VAR_DAY_OF_WEEK                                  0x40B9 // 0 = Sunday, 1 = Monday etc.
-#define VAR_UNUSED_0x40BA                                0x40BA // Unused
+#define VAR_DIFFICULTY                                   0x40BA // Sets difficulty of gym leaders and other important trainers by player hoice
 #define VAR_UNUSED_0x40BB                                0x40BB // Unused
 #define VAR_UNUSED_0x40BC                                0x40BC // Unused
 #define VAR_UNUSED_0x40BD                                0x40BD // Unused
