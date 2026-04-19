b10dfe9ab85026ecbe914a7d3222e9d1590d6169
Eemeliri <eemeli.rintanen@gmail.com>
2026-04-19 19:49:59 +0300

    marts now have TWO cashiers

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -715,7 +715,7 @@
 #define FLAG_HIDE_BATTLE_FRONTIER_RECEPTION_GATE_SCOTT  0x29C // Unused Flag
 #define FLAG_SYS_BUG_CONTEST_MODE  0x29D // Unused Flag
 #define FLAG_HM_EXPLANATION      0x29E
-#define FLAG_UNUSED_0x29F  0x29F // Unused Flag
+#define FLAG_LEVEL_SCALING_ON  0x29F
 #define FLAG_NAMED_SILVER  0x2A0 // Unused Flag
 #define FLAG_SLOWPOKE_TRADE    0x2A1
 #define FLAG_ROUTE31_EXPERT  0x2A2 // Flag for defeating Route 31 expert
