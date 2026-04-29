89de13f2cf3d454ff9a5caa7be45f98f4bc5a8bc
Eemeliri <eemeli.rintanen@gmail.com>
2026-04-29 10:31:15 +0200

    remove dead code from starter mega choice func

--- src/field_specials.c
+++ src/field_specials.c
@@ -4376,48 +4376,30 @@ void GetStarterMegaStone(void)
     switch (VarGet(VAR_PLAYER_STARTER_SPECIES))
     {
     case SPECIES_CHESPIN:
-    case SPECIES_QUILLADIN:
-    case SPECIES_CHESNAUGHT:
         gSpecialVar_Result = ITEM_CHESNAUGHTITE;
         break;
     case SPECIES_FENNEKIN:
-    case SPECIES_BRAIXEN:
-    case SPECIES_DELPHOX:
         gSpecialVar_Result = ITEM_DELPHOXITE;
         break;
     case SPECIES_FROAKIE:
-    case SPECIES_FROGADIER:
-    case SPECIES_GRENINJA:
         gSpecialVar_Result = ITEM_GRENINJITE;
         break;
     case SPECIES_CHIKORITA:
-    case SPECIES_BAYLEEF:
-    case SPECIES_MEGANIUM:
         gSpecialVar_Result = ITEM_MEGANIUMITE;
         break;
     case SPECIES_TOTODILE:
-    case SPECIES_CROCONAW:
-    case SPECIES_FERALIGATR:
         gSpecialVar_Result = ITEM_FERALIGITE;
         break;
     case SPECIES_CYNDAQUIL:
-    case SPECIES_QUILAVA:
-    case SPECIES_TYPHLOSION:
         gSpecialVar_Result = ITEM_TYPHLOSIONITE;
         break;
     case SPECIES_SPRIGATITO:
-    case SPECIES_FLORAGATO:
-    case SPECIES_MEOWSCARADA:
         gSpecialVar_Result = ITEM_MEOWSCARADITE;
         break;
     case SPECIES_TORCHIC:
-    case SPECIES_COMBUSKEN:
-    case SPECIES_BLAZIKEN:
         gSpecialVar_Result = ITEM_BLAZIKENITE;
         break;
     case SPECIES_POPPLIO:
-    case SPECIES_BRIONNE:
-    case SPECIES_PRIMARINA:
         gSpecialVar_Result = ITEM_PRIMARINITE;
         break;
     default:
