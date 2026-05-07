03a1ba0cc5370605f08d7f3292c03072579cc7bc
Eemeliri <eemeli.rintanen@gmail.com>
2026-05-07 14:43:05 +0200

    add opposite gender rival first scene from hgss

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1534,7 +1534,7 @@
 #define FLAG_HIDE_UXIE                              (SYSTEM_FLAGS + 0x9C)
 #define FLAG_HIDE_AZELF                             (SYSTEM_FLAGS + 0x9D)
 #define FLAG_HIDE_MESPRIT                           (SYSTEM_FLAGS + 0x9E)
-#define FLAG_UNUSED_0x8FF                           (SYSTEM_FLAGS + 0x9F) // Unused Flag
+#define FLAG_HIDE_NEWBARK_RIVALMARILL               (SYSTEM_FLAGS + 0x9F)
 #define FLAG_UNUSED_0x900                           (SYSTEM_FLAGS + 0xA0) // Unused Flag
 #define FLAG_UNUSED_0x901                           (SYSTEM_FLAGS + 0xA1) // Unused Flag
 #define FLAG_UNUSED_0x902                           (SYSTEM_FLAGS + 0xA2) // Unused Flag
--- include/constants/vars.h
+++ include/constants/vars.h
@@ -239,7 +239,7 @@
 #define VAR_BATTLE_TOWER_STREAK                          0x40DB
 #define VAR_BATTLE_SPEED                                 0x40DC
 #define VAR_GIFT_PICHU_SLOT                              0x40DD
-#define VAR_GIFT_UNUSED_1                                0x40DE // Var is written to, but never read
+#define VAR_RIVAL_STATE                                  0x40DE
 #define VAR_GIFT_UNUSED_2                                0x40DF // Var is written to, but never read
 #define VAR_GIFT_UNUSED_3                                0x40E0 // Var is written to, but never read
 #define VAR_GIFT_UNUSED_4                                0x40E1 // Var is written to, but never read
