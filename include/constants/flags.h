ae4fc1cd1060a58da729a83a65d549316bfe11e8
Eemeliri <eemeli.rintanen@gmail.com>
2026-05-09 19:06:59 +0200

    bugfixing, darkrai event script started, expand cherrygrove, safari zone giveaway

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1538,8 +1538,8 @@
 #define FLAG_NO_WHITEOUT                            (SYSTEM_FLAGS + 0xA0)
 #define FLAG_HIDE_CRESSELIA                         (SYSTEM_FLAGS + 0xA1)
 #define FLAG_HIDE_RIVAL_VIOLETGATEHOUSE             (SYSTEM_FLAGS + 0xA2)
-#define FLAG_UNUSED_0x903                           (SYSTEM_FLAGS + 0xA3) // Unused Flag
-#define FLAG_UNUSED_0x904                           (SYSTEM_FLAGS + 0xA4) // Unused Flag
+#define FLAG_PICKED_ZIGZAGOON                       (SYSTEM_FLAGS + 0xA3)
+#define FLAG_PICKED_RATTATA                         (SYSTEM_FLAGS + 0xA4)
 #define FLAG_UNUSED_0x905                           (SYSTEM_FLAGS + 0xA5) // Unused Flag
 #define FLAG_UNUSED_0x906                           (SYSTEM_FLAGS + 0xA6) // Unused Flag
 #define FLAG_UNUSED_0x907                           (SYSTEM_FLAGS + 0xA7) // Unused Flag
