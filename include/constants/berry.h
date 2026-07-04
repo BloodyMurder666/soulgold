f2e276f5109c90f167a9fb0d4a4cd2ba8a0de346
Eemeliri <eemeli.rintanen@gmail.com>
2026-07-04 20:36:33 +0300

    rest of the noted bugfixes

--- include/constants/berry.h
+++ include/constants/berry.h
@@ -53,10 +53,10 @@ enum __attribute__((__packed__)) Flavor
 // IDs for berry tree objects, indexes into berryTrees in SaveBlock1
 // Named for whatever berry is initially planted there on a new game
 // Those with no initial berry are named "soil"
-#define BERRY_TREE_ROUTE_102_PECHA    1
+#define BERRY_TREE_STARF_1    1
 #define BERRY_TREE_ROUTE_102_ORAN     2
-#define BERRY_TREE_ROUTE_104_SOIL_1   3
-#define BERRY_TREE_ROUTE_104_ORAN_1   4
+#define BERRY_TREE_LANSAT_1   3
+#define BERRY_TREE_MICLE_1   4
 #define BERRY_TREE_ROUTE_103_CHERI_1  5
 #define BERRY_TREE_ROUTE_103_LEPPA    6
 #define BERRY_TREE_ROUTE_103_CHERI_2  7
