cbdef064d2188d6b173c831dc92ed5ad4fc833fc
Eemeliri <eemeli.rintanen@gmail.com>
2026-07-04 21:18:23 +0300

    adjust clair script placement to be in line with HGSS, fix tutor move from box display, draco meteor tutor, give shiny dratini with spacial rend

--- src/pokemon_storage_system.c
+++ src/pokemon_storage_system.c
@@ -5017,6 +5017,8 @@ static u8 CreateBoxMonIconsInColumn(u8 column, u16 distance, s16 speed)
                     sStorage->boxMonsSprites[boxPosition]->sScrollInDestX = xDest;
                     sStorage->boxMonsSprites[boxPosition]->callback = SpriteCB_BoxMonIconScrollIn;
                     SetBoxMonDynamicPalette(sStorage->scrollToBoxId, boxPosition);
+                    if (ShouldBoxmonSpriteBeTransparent(sStorage->incomingBoxId, boxPosition))
+                        sStorage->boxMonsSprites[boxPosition]->oam.objMode = ST_OAM_OBJ_BLEND;
                     iconsCreated++;
                 }
             }
