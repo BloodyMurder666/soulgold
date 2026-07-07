b70dfdbecae6600c6af7635c8d6d4113e9513fef
Eemeliri <eemeli.rintanen@gmail.com>
2026-07-07 11:07:49 +0300

    allow dexnav in dark caves by setting flash level to 0 after obtaining flash

--- src/overworld.c
+++ src/overworld.c
@@ -1077,7 +1077,7 @@ void SetDefaultFlashLevel(void)
     if (!gMapHeader.cave)
         gSaveBlock1Ptr->flashLevel = 0;
     else if (FlagGet(FLAG_SYS_USE_FLASH))
-        gSaveBlock1Ptr->flashLevel = 1;
+        gSaveBlock1Ptr->flashLevel = 0;
     else
         gSaveBlock1Ptr->flashLevel = gMaxFlashLevel - 1;
 }
