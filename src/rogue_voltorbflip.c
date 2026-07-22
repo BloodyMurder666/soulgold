67319ad42052cb4136617b17ce772ddc7d54820f
Eemeliri <eemeli.rintanen@gmail.com>
2026-07-22 22:54:41 +0300

    fix voltorb flip in release build

--- src/rogue_voltorbflip.c
+++ src/rogue_voltorbflip.c
@@ -1113,7 +1113,7 @@ static const u8 sTextGrey[] = _("{COLOR LIGHT_GRAY}{SHADOW DARK_GRAY}");
 
 static void PrintRowInfo(u8 window, u8 totalValue, u8 totalVoltorbs)
 {
-    u8 text[8];
+    u8 text[max(ARRAY_COUNT(sTextWhite), ARRAY_COUNT(sTextGrey)) + 2];
     FillWindowPixelBuffer(window, PIXEL_FILL(0));
 
     // Top number
